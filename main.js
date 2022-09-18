104ce02e639298eac83f3769a29ebbecd2e43e23
sneaky-design <85828142+sneaky-design@users.noreply.github.com>
2022-09-18 10:54:48 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -20,6 +20,29 @@ var ball = {
     dx:3,
     dy:3
 }
+
+function setup(){
+  canvas =  createCanvas(700,600);
+
+  video= createCapture(VIDEO);
+  video.size(800,400);
+
+  poseNet= ml5.poseNet(video, modelLoaded);
+  poseNet.on("pose",gotPoses);
+  video.parent('canvas');
+}
+function modelLoaded(){
+  console.log("model Loaded.");
+}
+function gotPoses(results){
+  if(results.length > 0){
+      console.log(results);
+      rightwristX=results[0].pose.right.wrist.X
+      rightwristY=results[0].pose.right.wrist.Y
+      console.log(results)
+  }
+}
+
 function draw(){
 
  background(0); 
